e9eb9638fd869d8d445bc1f2ea6b49973ae4dd28
Aarogya <aarogyathapa375@gmail.com>
2026-03-07 21:59:46 +0545

    Update on the non-tail code implementation

--- src/RecursionFunction/NonTailRecursion.java
+++ src/RecursionFunction/NonTailRecursion.java
@@ -8,8 +8,11 @@ public class NonTailRecursion {
 	  if(n == 0) {
 		  return 1;
 	  }
-	  else if(n<0) {
+	  if(n<0) {
 		  System.out.println("Enter any number bigger than 0");
+		  	
+		  
+		  return n;
 	  }
 	  
 		  return n * factorial(n - 1);
@@ -24,7 +27,7 @@ public class NonTailRecursion {
 	
 	
 	
-	System.out.println("Factorial (6): "+ f1.factorial(-1));
+	System.out.println("Factorial (6): "+ f1.factorial(16));
 }
 
 }
