b1b232dec189464c319707221a32f13d9835eb1b
Aarogya <aarogyathapa375@gmail.com>
2026-03-12 23:28:09 +0545

    Update on the core dsa implementation i.e. recursion implementation

---
+++ src/RecursionFunction/recursionFunctions.java
@@ -0,0 +1,17 @@
+public class recursionFunction {
+
+    // Recursive method to find factorial
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {   // Base case
+            return 1;
+        }
+        return n * factorial(n - 1);  // Recursive call
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        int result = factorial(num);
+
+        System.out.println("Factorial of " + num + " is: " + result);
+    }
+}
